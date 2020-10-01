8e9f8afa9eac64a009b65cdc5b2d8c716cf85217
Gregory.Johnson <3531op@gmail.com>
2020-10-01 15:00:01 -0400

    Handing it over to Sonny

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ const isValidSecondary = require('./is-valid-secondary.js')
 
 
 // Your code here!
+
+const userInput1 = process.argv[2];
+const userInput2 = process.argv[3];
+
+if (userInput1 === undefined && userInput2 === undefined) {
+  console.log("Must be a valid color")
+}
+
+if (userInput1 !== undefined && userInput2 === undefined) {
+  const valid = colorDeconstructor(userInput1);
+  console.log("You entered " + input1 + " & you received " + valid)
+}
+
+if (userInput1 !== undefined && userInput2 !== undefined)
+  const comBine = 
